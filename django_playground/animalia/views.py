7cca4ffb405b2bdae3a28e2208d56b70e70f23bf
Timothy Asiimwe <atimothee@gmail.com>
2014-11-10 23:21:28 +0300

    styled animal detail & added related animals

--- django_playground/animalia/views.py
+++ django_playground/animalia/views.py
@@ -16,11 +16,11 @@ def home(request):
         animals = paginator.page(1)
     except EmptyPage:
         animals = paginator.page(paginator.num_pages)
-    return render_to_response('animalia/home.html', {'popular': animals}, RequestContext(request))
+    return render_to_response('animalia/home.html', {'popular': animals, 'home_active': 'active'}, RequestContext(request))
 
 def animal_list(request):
     animals = Animal.objects.filter(name__icontains=request.GET.get('name', ''), species__genus__family__name__icontains=request.GET.get('family', ''))
-    return render_to_response('animalia/animal_list.html', {'animals': animals})
+    return render_to_response('animalia/animal_list.html', {'animals': animals, 'catalogue_active':'active'})
 
 class AnimalDetail(DetailView):
     model = Animal
@@ -32,3 +32,10 @@ class AnimalDetail(DetailView):
             return Animal.objects.get(pk=pk)
         except Animal.DoesNotExist:
             raise Http404
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        pk = self.kwargs.get(self.pk_url_kwarg, None)
+        obj = Animal.objects.get(pk=pk)
+        context = super(AnimalDetail, self).get_context_data(**kwargs)
+        context['related'] = Animal.objects.filter(species__genus__family_id=obj.species.genus.family_id)[:3]  # #look into proper filter using ORs
+        return context
